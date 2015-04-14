1af274e84e642cff2be3f9978075dc154fa3889a
kbz <kebingzao@gmail.com>
2015-04-14 15:31:40 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+
+
+var session = require('express-session');
+app.use(session({
+  // 建议使用 128 个字符的随机字符串
+  secret: 'recommand 128 bytes random string', 
+  cookie: { maxAge: 60 * 1000 }
+}));
+app.get('/', function (req, res) {
+  // 检查 session 中的 isVisit 字段
+  // 如果存在则增加一次，否则为 session 设置 isVisit 字段，并初始化为 1。
+  if(req.session.isVisit) {
+    req.session.isVisit++;
+    res.send('<p>第 ' + req.session.isVisit + '次来此页面</p>');
+  } else {
+    req.session.isVisit = 1;
+    res.send("欢迎第一次来这里");
+    console.log(req.session);
+  }
+});
+
+
+
+
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(4556);
+
+module.exports = app;
